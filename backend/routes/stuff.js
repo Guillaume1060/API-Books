07722f62dc84c82ba2d3a007d1017ae8f38e1964
guillaume1060 <breyerguillaume@gmail.com>
2022-09-05 16:44:55 +0200

    init front

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const auth = require('../middleweare/auth')
+// const auth = require('../middleweare/auth')
 const router = express.Router();
 
 
 
 const stuffCtrl = require('../controllers/stuff');
 
-router.get('/',auth, stuffCtrl.getAllStuff);
-router.post('/',auth, stuffCtrl.createbook);
-router.get('/:id',auth, stuffCtrl.getOnebook);
-router.put('/:id',auth, stuffCtrl.modifybook);
-router.delete('/:id',auth, stuffCtrl.deletebook);
+router.get('/', stuffCtrl.getAllStuff);
+router.post('/', stuffCtrl.createbook);
+router.get('/:id', stuffCtrl.getOnebook);
+router.put('/:id', stuffCtrl.modifybook);
+router.delete('/:id', stuffCtrl.deletebook);
 
 module.exports = router;
